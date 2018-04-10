a962a7a801e550873783e1f87cdcbacafc98e131
frankygg <b97501078@gmail.com>
2018-04-10 11:10:11 +0800

    first commit

---
+++ Supercool/ViewController.swift
@@ -0,0 +1,105 @@
+//
+//  ViewController.swift
+//  Supercool
+//
+//  Created by BoTingDing on 2018/4/9.
+//  Copyright © 2018年 BoTingDing. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuthUI
+
+class ViewController: UIViewController, FUIAuthDelegate {
+
+    //database
+    var ref: DatabaseReference!
+    
+    var user: User!
+    
+    @IBAction func logout(_ sender: Any) {
+//        do {
+//            let authUI = FUIAuth.defaultAuthUI()
+//
+//            try authUI?.signOut()
+//
+//        } catch {
+//            print("sth went wrong")
+//        }
+        
+        
+        ref = Database.database().reference()
+        var article = ["article" : "life", "content": "is", "tag": "a shit"]
+        
+        ref?.child("User").child(user!.uid).setValue(article)
+        
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let authUI = FUIAuth.defaultAuthUI()
+
+        // You need to adopt a FUIAuthDelegate protocol to receive callback
+        authUI?.delegate = self
+        
+        //create user
+        Auth.auth().createUser(withEmail: "456@123.123.123", password: "123456") { (user, error) in
+            // ...
+            if error == nil && user != nil {
+                print("sign up")
+                print("name = \(user?.displayName), email = \(user?.email)")
+
+            }else {
+                print("error creating user \(error?.localizedDescription)")
+            }
+        }
+        
+        //sign in user
+        Auth.auth().signIn(withEmail: "456@123.123.123", password: "123456"){ (user, error) in
+                        // ...
+                        if error == nil && user != nil {
+                            print("sign in")
+                            print("name = \(user?.displayName), email = \(user?.email)")
+                            self.user = user
+
+                        }else {
+                            print("error creating user \(error?.localizedDescription)")
+                        }
+                    }
+
+        
+        
+       
+      
+
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith user: User?, error: Error?) {
+        // handle user and error as necessary
+        if error == nil {
+            
+            print("sign with \(user?.email)")
+        }else {
+            print("sign in with error \(error?.localizedDescription)")
+        }
+       
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+struct Article {
+    var title: String
+    var content: String
+    var tag: String
+}
+
